835e9f050a0473021da301d38b33ce8d981b1fb9
Valentyn <valentynejk@gmail.com>
2022-09-14 16:18:02 +0200

    Recursive_Digit_array

---
+++ Recursive_Digit_sum.js
@@ -0,0 +1,15 @@
+function superDigit(n, k) {
+  if (n < 10) return n
+
+  let returnNumber = n.toString().split('').reduce((acc, curr) => acc + parseInt(curr), 0) * k;
+  let arrayN = returnNumber.toString().split('')
+
+  while (returnNumber > 9) {
+    returnNumber = arrayN.reduce((acc, curr) => acc + parseInt(curr), 0);
+    arrayN = returnNumber.toString().split('')
+  }
+  return returnNumber;
+}
+
+console.log(superDigit(9875, 4));
+
